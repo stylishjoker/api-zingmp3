bed89e188ae8fc47fb2f0b150c7fdb185dd7b220
stylishjoker <111648007+stylishjoker@users.noreply.github.com>
2024-01-09 10:36:32 +0700

    new update

--- src/controllers/ZingController.js
+++ src/controllers/ZingController.js
@@ -1,91 +1,106 @@
-const { ZingMp3 } = require("zingmp3-api-full")
+const { ZingMp3 } = require("zingmp3-api-full");
 
 class ZingController {
+  getSong(req, res) {
+    ZingMp3.getSong(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
 
-    getSong(req, res) {
-        ZingMp3.getSong(req.query.id).then((data) => {
-            res.json(data)
-        })
+  getDetailPlaylist(req, res) {
+    ZingMp3.getDetailPlaylist(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getHome(req, res) {
+    ZingMp3.getHome().then((data) => {
+      res.json(data);
+    });
+  }
+
+  getTop100(req, res) {
+    ZingMp3.getTop100().then((data) => {
+      res.json(data);
+    });
+  }
+
+  getChartHome(req, res) {
+    ZingMp3.getChartHome().then((data) => {
+      res.json(data);
+    });
+  }
+
+  getNewReleaseChart(req, res) {
+    ZingMp3.getNewReleaseChart().then((data) => {
+      res.json(data);
+    });
+  }
+
+  getInfo(req, res) {
+    ZingMp3.getInfoSong(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getArtist(req, res) {
+    ZingMp3.getArtist(req.query.name).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getArtistSong(req, res) {
+    ZingMp3.getListArtistSong(
+      req.query.id,
+      req.query.page,
+      req.query.count
+    ).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getLyric(req, res) {
+    ZingMp3.getLyric(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
+
+  search(req, res) {
+    ZingMp3.search(req.query.keyword).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getListMV(req, res) {
+    ZingMp3.getListMV(req.query.id, req.query.page, req.query.count).then(
+      (data) => {
+        res.json(data);
+      }
+    );
+  }
+
+  getCategoryMV(req, res) {
+    ZingMp3.getCategoryMV(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
+
+  getVideo(req, res) {
+    ZingMp3.getVideo(req.query.id).then((data) => {
+      res.json(data);
+    });
+  }
+  async getSongV2(req, res) {
+    const { id, sig, ctime, version, apiKey } = req.query;
+    const response = await fetch(
+      `https://zingmp3.vn/api/v2/song/get/streaming?id=${id}&sig=${sig}&ctime=${ctime}&version=${version}&apiKey=${apiKey}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-
-    getDetailPlaylist(req, res) {
-        ZingMp3.getDetailPlaylist(req.query.id).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getHome(req, res) {
-        ZingMp3.getHome().then((data) => {
-            res.json(data)
-        })
-    }
-
-    getTop100(req, res) {
-        ZingMp3.getTop100().then((data) => {
-            res.json(data);
-        })
-    }
-
-    getChartHome(req, res) {
-        ZingMp3.getChartHome().then((data) => {
-            res.json(data);
-        })
-    }
-
-    getNewReleaseChart(req, res) {
-        ZingMp3.getNewReleaseChart().then((data) => {
-            res.json(data)
-        })
-    }
-
-    getInfo(req, res) {
-        ZingMp3.getInfoSong(req.query.id).then((data) => {
-            res.json(data);
-        })
-    }
-
-    getArtist(req, res) {
-        ZingMp3.getArtist(req.query.name).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getArtistSong(req, res) {
-        ZingMp3.getListArtistSong(req.query.id, req.query.page, req.query.count).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getLyric(req, res) {
-        ZingMp3.getLyric(req.query.id).then((data) => {
-            res.json(data)
-        })
-    }
-
-    search(req, res) {
-        ZingMp3.search(req.query.keyword).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getListMV(req, res) {
-        ZingMp3.getListMV(req.query.id, req.query.page, req.query.count).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getCategoryMV(req, res) {
-        ZingMp3.getCategoryMV(req.query.id).then((data) => {
-            res.json(data)
-        })
-    }
-
-    getVideo(req, res) {
-        ZingMp3.getVideo(req.query.id).then((data) => {
-            res.json(data)
-        })
-    }
-
+    const data = await response.json();
+    res.json(data);
+  }
 }
 
-module.exports = new ZingController
+module.exports = new ZingController();
--- src/routes/ZingRouter.js
+++ src/routes/ZingRouter.js
@@ -1,48 +1,50 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const ZingController = require("../controllers/ZingController")
+const ZingController = require("../controllers/ZingController");
 
 // getSong
-router.get("/song", ZingController.getSong)
+router.get("/song", ZingController.getSong);
 
 // getDetailPlaylist
-router.get("/detailplaylist", ZingController.getDetailPlaylist)
+router.get("/detailplaylist", ZingController.getDetailPlaylist);
 
 // getHome
-router.get("/home", ZingController.getHome)
+router.get("/home", ZingController.getHome);
 
 // getTop100
-router.get("/top100", ZingController.getTop100)
+router.get("/top100", ZingController.getTop100);
 
 // getChartHome
-router.get("/charthome", ZingController.getChartHome)
+router.get("/charthome", ZingController.getChartHome);
 
 // getNewReleaseChart
-router.get("/newreleasechart", ZingController.getNewReleaseChart)
+router.get("/newreleasechart", ZingController.getNewReleaseChart);
 
 // getInfoSong
-router.get("/infosong", ZingController.getInfo)
+router.get("/infosong", ZingController.getInfo);
 
 // getArtist
-router.get("/artist", ZingController.getArtist)
+router.get("/artist", ZingController.getArtist);
 
 // getArtistSong
-router.get("/artistsong", ZingController.getArtistSong)
+router.get("/artistsong", ZingController.getArtistSong);
 
 // getLyric
-router.get("/lyric", ZingController.getLyric)
+router.get("/lyric", ZingController.getLyric);
 
 // search
-router.get("/search", ZingController.search)
+router.get("/search", ZingController.search);
 
 // getListMV
-router.get("/listmv", ZingController.getListMV)
+router.get("/listmv", ZingController.getListMV);
 
 // getCategoryMV
-router.get("/categorymv", ZingController.getCategoryMV)
+router.get("/categorymv", ZingController.getCategoryMV);
 
 // getVideo
-router.get("/video", ZingController.getVideo)
+router.get("/video", ZingController.getVideo);
 
-module.exports = router
+router.get("/songV2", ZingController.getSongV2);
+
+module.exports = router;
